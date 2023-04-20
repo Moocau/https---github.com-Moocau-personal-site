84534db030eae51ceb6ceabf97c1792181280e13
Robyn Snook <snook.robyn@gmail.com>
2023-04-20 11:22:43 +0900

    feat: basic navbar & styling setup

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export const Navbar = () => {
+
+  return (
+    <div className='navigation-bar'>
+      <span>Home</span>
+      <span>About</span>
+      <span>Projects</span>
+      <span>Blog</span>
+    </div>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.scss';
+import type { AppProps } from 'next/app';
+import { Navbar } from '@/components/Navbar';
+import '../styles/Navbar.scss'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App() {
+  return <Navbar />
 }
