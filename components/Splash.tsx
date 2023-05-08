c50389bf5cab2af86828331c821075442f975253
Robyn Snook <112469690+Moocau@users.noreply.github.com>
2023-05-08 16:36:26 +0900

    chore: clear default format and change ratio image

--- components/Splash.tsx
+++ components/Splash.tsx
@@ -7,13 +7,15 @@ export const Splash = () => {
       <Image
         src='/splash-image.jpeg'
         alt='me'
-        width='738'
+        width='539'
         height='539'
         id='splash-image'
       />
       <div>
-        <h1>Greetings! I'm Robyn Snook</h1>
+        <h1>Greetings, I'm Robyn Snook</h1>
         <h2>Software Engineer by day, Food Connoisseuse by night</h2>
+        <p>Passionate about puzzle solving and building interesting things from code.</p>
+        <p>Always seeking to learn and grow.</p>
       </div>
     </div>
   );
