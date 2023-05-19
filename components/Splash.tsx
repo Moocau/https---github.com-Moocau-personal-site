30a3c0c4278413d6a60a7a3fee086b3d57dfc950
Robyn Snook <112469690+Moocau@users.noreply.github.com>
2023-05-19 14:56:04 +0900

    feat: modify entry text to suit themeing

--- components/Splash.tsx
+++ components/Splash.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import Image from 'next/image';
 
-
 export const Splash = () => {
-
-
   return (
-    <div className='splash-welcome-mat'>
-      <div className='splash-background background'></div>
+    <div className="splash-welcome-mat">
+      <div className="splash-background background"></div>
       <div className="splash-container">
         <div className="splash-message">
-          <h1>Greetings, I'm Robyn Snook</h1>
-          <h2>Software Engineer by day, Food Connoisseuse by night</h2>
+          <h1>I am Robyn Snook,</h1>
+          <h2>
+            A relentless navigator of the binary matrix, harnessing the power of
+            code by day. As twilight descends, I morph into a
+            culinary alchemist, savoring the hidden flavors of this neon-lit
+            metropolis.
+          </h2>
           <p>
-            Passionate about puzzle solving and building interesting things from
-            code.
+            I am driven by the unquenchable
+            thirst for deciphering enigmatic puzzles and constructing
+            awe-inspiring creations.
           </p>
-          <p>Always seeking to learn and grow.</p>
+          <p>
+            Venture forth with me into the boundless expanse of the dataverse,
+            where innovation and evolution converge. Together, we shall
+            transcend boundaries and unravel the mysteries that pulse through
+            the veins of the digital labyrinth.
+          </p>
         </div>
       </div>
     </div>
