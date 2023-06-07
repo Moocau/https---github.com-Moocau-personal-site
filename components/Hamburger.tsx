de94274fbd147164d750787b0dcce2b0cc6f3ff7
Robyn Snook <112469690+Moocau@users.noreply.github.com>
2023-06-07 11:52:48 +0900

    feat: separate hamburger to component

---
+++ components/Hamburger.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useState } from 'react';
+import Link from 'next/link';
+
+
+export const Hamburger = () => {
+  const [ showMenu, setShowMenu ] = useState<boolean>(false);
+
+  const addMenuEventListener = () => {
+    document.addEventListener('mousedown', hideMenu);
+    document.getElementById('menu')?.addEventListener('mousedown', stopEventBubbling);
+    document.getElementsByClassName('menu-button-container')[0].addEventListener('mousedown', stopEventBubbling as EventListener);
+  }
+  const removeMenuEventListener = () => {
+    document.removeEventListener('mousedown', hideMenu);
+    document.getElementById('menu')?.removeEventListener('mousedown', stopEventBubbling);
+    document.getElementsByClassName('menu-button-container')[0].removeEventListener('mousedown', stopEventBubbling as EventListener);
+  }
+  const toggleMenu = () => {
+    if (!showMenu) { addMenuEventListener() }
+    else { removeMenuEventListener() }
+    setShowMenu(!showMenu);
+  };
+  const hideMenu = () => {
+    setShowMenu(false);
+    removeMenuEventListener();
+  };
+  const stopEventBubbling = (e: MouseEvent) => e.stopPropagation();
+
+  return (
+    <div className='mobile-navigation-bar'>
+      <div className='mobile-navigation-items'>
+        <div className='navbar-item-inner'>
+          <Link href='/'>Robyn Snook</Link>
+        </div>
+        <button id="menu-toggle" className={showMenu ? 'menu-cross' : 'menu-hamburger'} onClick={toggleMenu}></button>
+        <label className="menu-button-container" htmlFor="menu-toggle">
+          <div className="menu-button"></div>
+        </label>
+        <ul id='hamburger' className='hamburger'>
+          <li><Link href='/about'>About</Link></li>
+          <li><Link href='/projects'>Projects</Link></li>
+          <li><Link href='/blog'>Blog</Link></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
 import { Navbar } from '@/components/Navbar';
 import { Footer } from '@/components/Footer';
 import { Splash } from '@/components/Splash';
+import { Hamburger } from '@/components/Hamburger';
 import { useRouter } from 'next/router';
 import '@/styles/globals.scss';
 import '../styles/Navbar.scss';
@@ -10,13 +12,28 @@ import '../styles/Splash.scss';
 import '../styles/Projects.scss';
 import '../styles/About.scss';
 import '../styles/Blog.scss';
+import '../styles/Hamburger.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
+  const [mobile, setMobile] = useState<boolean | undefined>(undefined)
+
+  useEffect(() => {
+    const updateMobile = () => {
+      setMobile(window.innerWidth < 700 ? true : false)
+    }
+
+    updateMobile()
+    window.addEventListener('resize', updateMobile)
+    return () => {
+      window.removeEventListener('resize', updateMobile)
+    }
+  }, [])
+
   return (
     <div className='page-container'>
-      <Navbar />
+      {typeof mobile !== 'undefined' ? (mobile? <Hamburger /> : <Navbar />) : null}
       {router.pathname === '/' && <Splash />}
       <Component {...pageProps} />
       <Footer />
